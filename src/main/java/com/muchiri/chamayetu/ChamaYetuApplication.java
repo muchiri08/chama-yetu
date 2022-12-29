bb0b3d2634343e9f4fa5c6b6c9a3f287faddc559
muchiri08 <mbogokennedy08@gmail.com>
2022-12-29 12:36:15 +0300

    initial commit

---
+++ src/main/java/com/muchiri/chamayetu/ChamaYetuApplication.java
@@ -0,0 +1,13 @@
+package com.muchiri.chamayetu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChamaYetuApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChamaYetuApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/muchiri/chamayetu/ChamaYetuApplicationTests.java
@@ -0,0 +1,13 @@
+package com.muchiri.chamayetu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChamaYetuApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
