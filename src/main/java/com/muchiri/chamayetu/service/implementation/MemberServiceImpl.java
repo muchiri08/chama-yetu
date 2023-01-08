c237b1cc99a7c94b12c361f35ecdb98b7afd6d53
muchiri08 <mbogokennedy08@gmail.com>
2023-01-08 23:37:16 +0300

    added sort desc to getAllMembers

--- src/main/java/com/muchiri/chamayetu/service/implementation/MemberServiceImpl.java
+++ src/main/java/com/muchiri/chamayetu/service/implementation/MemberServiceImpl.java
@@ -10,7 +10,9 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.modelmapper.ModelMapper;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -33,7 +35,9 @@ public class MemberServiceImpl implements MemberService {
 
     @Override
     public Page<MemberDto> getAllMembers(Pageable pageable) throws PageNotFoundException, MemberNotFoundException {
-        Page<Member> members = memberRepository.findAll(pageable);
+        Sort sort = Sort.by(Sort.Direction.DESC, "id");
+        Pageable sortedPageable = PageRequest.of(pageable.getPageNumber(), pageable.getPageSize(), sort);
+        Page<Member> members = memberRepository.findAll(sortedPageable);
         int totalPages = members.getTotalPages();
 
         if (pageable.getPageSize() < 0 || pageable.getPageNumber() > totalPages) {
