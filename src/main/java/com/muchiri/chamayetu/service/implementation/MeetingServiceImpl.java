6a9d8fbdfe64d6e2c44d81db11066fd6140b2cd7
muchiri08 <mbogokennedy08@gmail.com>
2023-01-09 22:19:34 +0300

    created meeting service and controller

---
+++ src/main/java/com/muchiri/chamayetu/service/implementation/MeetingServiceImpl.java
@@ -0,0 +1,8 @@
+package com.muchiri.chamayetu.service.implementation;
+
+import com.muchiri.chamayetu.service.interfaces.MeetingService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class MeetingServiceImpl implements MeetingService {
+}
