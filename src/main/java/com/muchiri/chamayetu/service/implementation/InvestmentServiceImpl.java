5aa7cec3aab0b405a7663ec2c67e5eff12e76121
muchiri08 <mbogokennedy08@gmail.com>
2023-01-10 15:13:02 +0300

    created investment service and controller

---
+++ src/main/java/com/muchiri/chamayetu/service/implementation/InvestmentServiceImpl.java
@@ -0,0 +1,8 @@
+package com.muchiri.chamayetu.service.implementation;
+
+import com.muchiri.chamayetu.service.interfaces.InvestmentService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class InvestmentServiceImpl implements InvestmentService {
+}
