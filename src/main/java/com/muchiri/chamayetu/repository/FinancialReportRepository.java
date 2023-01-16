97c493120b2e016b9e26a926e661c412ea51bb02
muchiri08 <mbogokennedy08@gmail.com>
2023-01-16 15:48:25 +0300

    deleted financial report, financial report data, their repos

--- src/main/java/com/muchiri/chamayetu/repository/FinancialReportRepository.java
+++
@@ -1,9 +0,0 @@
-package com.muchiri.chamayetu.repository;
-
-import com.muchiri.chamayetu.entity.FinancialReport;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface FinancialReportRepository extends JpaRepository<FinancialReport, Long> {
-}
